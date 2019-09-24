936ecd614e5f63a494034b31b4921d33ef8731c9
Caleb Flanagan <caleb.flanagan@clearlink.com>
2019-09-23 22:04:25 -0600

    adding some of the javascript logic
    Signed-off-by: Caleb Flanagan <caleb.flanagan@clearlink.com>

--- site.js
+++ site.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
 
+  // STORE THE BUTTON IN A VARIABLE TO BE USED
   const button = document.querySelector('button');
 
+  // WHEN THE BUTTON IS CLICKED, STORE NUMBER OF ROWS AND COLUMNS
   button.addEventListener('click', function () {
-    alert('the button was clicked');
+    let rows = window.prompt('How many rows would you like?');
+    let columns = window.prompt('How many columns would you like?');
+
+    // IF NO VALUE IS ADDED TO PROMPT, DEFAULT TO VALUE OF '12'
+    if (rows === '') {rows = 12;}
+    if (columns === '') {columns = 12;}
+
+    alert(`rows = ${rows} : columns = ${columns}`);
   });
 
 });
